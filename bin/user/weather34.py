aeb4592fd38d3f59b4722d19e5a2110ea9ceb810
Ian Millard <steepleian@gmail.com>
2019-11-22 17:14:07 +0000

    Updated to include to include retention of loop packets

--- bin/user/weather34.py
+++ bin/user/weather34.py
@@ -356,9 +356,39 @@ class ZambrettiForecast():
     @staticmethod
     def alpha_to_number(x):
         return ord(x) - 64
+      
+class RetainLoopValues(StdService):
+    """Service retains previous loop packet values updating any value that isn't None from new
+    packets. It then replaces the original packet with a new packet that contains all of the values; 
+    the original unmodified packet will be stored on the event in a property named 'originalPacket'."""
+
+    def __init__(self, engine, config_dict):
+        super(RetainLoopValues, self).__init__(engine, config_dict)
+        self.bind(weewx.NEW_LOOP_PACKET, self.newLoopPacket)
+        self.retainedLoopValues = {}
+        self.excludeFields = set([])
+        if 'RetainLoopValues' in config_dict:
+            if 'exclude_fields' in config_dict['RetainLoopValues']:
+                self.excludeFields = set(weeutil.weeutil.option_as_list(config_dict['RetainLoopValues'].get('exclude_fields', [])))
+                syslog.syslog(syslog.LOG_INFO, "RetainLoopValues: excluding fields: %s" % (self.excludeFields,))
+
+    def newLoopPacket(self, event):
+        event.originalPacket = event.packet
+        # replace the values in the retained packet if they have a value other than None or the field is listed in excludeFields
+        self.retainedLoopValues.update( dict((k,v) for k,v in event.packet.iteritems() if (v is not None or k in self.excludeFields)) )
+        # if the new packet doesn't contain one of the excludeFields then remove it from the retainedLoopValues
+        for k in self.excludeFields - set(event.packet.keys()):
+            if k in self.retainedLoopValues:
+                self.retainedLoopValues.pop(k)
+        event.packet = self.retainedLoopValues.copy()      
 
 
 class Weather34RealTime(StdService):
+  
+ 
+
+  
+  
 
     def __init__(self, engine, config_dict):
         super(Weather34RealTime, self).__init__(engine, config_dict)
@@ -427,7 +457,7 @@ class Weather34RealTime(StdService):
             data = self.calculate(event_data, dbm)
             self.write_data(data)
         except Exception, e:
-            logdbg("crt: Exception while handling data: %s" % e)
+            logdbg("w34rt: Exception while handling data: %s" % e)
             weeutil.weeutil.log_traceback('crt: **** ')
             raise
 
